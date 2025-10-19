454a4ea6881f6906dac0c767c5d0922be5c6eaf0
German MSI <armendariz.german@outlook.com>
2025-10-19 16:51:43 -0600

    Nav

--- views/index.view.php
+++ views/index.view.php
@@ -39,13 +39,7 @@
                         <a class="nav-link" href="#Contacto">Contacto</a>
                     </li>
 
-                    <li class="nav-item">
-                        <a class="nav-link" href="login.php">Login</a>
-                    </li>
 
-                    <li class="nav-item">
-                        <a class="nav-link" href="privacy-policy.php">Aviso de Privacidad</a>
-                    </li>
 
                 </ul>
             </div>
